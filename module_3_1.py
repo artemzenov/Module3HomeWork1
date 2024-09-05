84dacca2f6d21af4ab3f0f79723c462c41128a86
Artem Zenov <zenovae1987@gmail.com>
2024-09-05 21:08:14 +0500

    Add module_3_1.py

---
+++ module_3_1.py
@@ -0,0 +1,26 @@
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+
+    for index_elem in range(len(list_to_search)):
+        list_to_search[index_elem] = list_to_search[index_elem].lower()
+
+    return string.lower() in list_to_search
+
+
+calls = 0
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
